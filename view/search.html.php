a540c90087a449c939b51ae84251844d987891ad
maulikchevli <maulikchevli@gmail.com>
2018-11-17 18:10:08 +0530

    Delete 'Rank' from search results

--- view/search.html.php
+++ view/search.html.php
@@ -83,7 +83,8 @@
 		foreach($search->dosearch($_GET['q']) as $result) {
 			?>
 				<li>
-					<a href="<?php echo $result[0]; ?>"><?php echo preg_replace('/[^(\x20-\x7F)]*/','',$result[1]); ?></a> <small>Rank: <?php echo $result[3]; ?></small><br>
+					<a href="<?php echo $result[0]; ?>"><?php echo preg_replace('/[^(\x20-\x7F)]*/','',$result[1]); ?></a> 
+					<br>
 					<a style="color:#093; text-decoration:none;" href="<?php echo $result[0]; ?>"><?php echo $result[0]; ?></a>
 					<p><?php echo preg_replace('/[^(\x20-\x7F)]*/','',$result[2]); ?></p>
 				</li>
